d858a642873a7b596aa4434427162c1ff5f4ecf1
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-10 17:50:39 +0200

    add a page for adding products to the catalog

---
+++ HibernatingRhinos.Orders.Backend/Features/Products/AddProducts.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace HibernatingRhinos.Orders.Backend.Features.Products
+{
+    public partial class AddProduct : Page
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
