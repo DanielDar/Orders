0bd6b20661a264d0a9cca25e65948f7e9af8a4b7
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-10 17:48:21 +0200

    Converters for the list of store id's

---
+++ HibernatingRhinos.Orders.Backend/Converters/ArrayListToMultiLineStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Linq;
+
+namespace HibernatingRhinos.Orders.Backend.Converters
+{
+    public class ArrayListToMultiLineStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Join(Environment.NewLine, (IEnumerable<string>) value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string) value).Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
---
+++ HibernatingRhinos.Orders.Backend/Converters/ArrayListToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Linq;
+
+namespace HibernatingRhinos.Orders.Backend.Converters
+{
+    public class ArrayListToStringConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Join(", ", (IEnumerable<string>)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string)value).Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
