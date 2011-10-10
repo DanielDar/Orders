51f61e26c14c253c2f9a08396c63ebb3529a386c
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-10 17:47:40 +0200

    Initialize Project

---
+++ HibernatingRhinos.Orders.Backend/RavenDB.cs
@@ -0,0 +1,18 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+
+namespace HibernatingRhinos.Orders.Backend
+{
+    public static class RavenDB
+    {
+        static RavenDB()
+        {
+            Store = new DocumentStore
+            {
+                Url = "http://localhost:8080"
+            }.Initialize();
+        }
+
+        public static IDocumentStore Store { get; private set; }
+    }
+}
