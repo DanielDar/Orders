a833dabc323f339fe7fbe5037f78d5ab04f5d38a
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-11 12:17:25 +0200

    Fix NRE

--- HibernatingRhinos.Orders.Backend/Converters/ArrayListToMultiLineStringConverter.cs
+++ HibernatingRhinos.Orders.Backend/Converters/ArrayListToMultiLineStringConverter.cs
@@ -10,6 +10,8 @@ namespace HibernatingRhinos.Orders.Backend.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null)
+                return null;
             return string.Join(Environment.NewLine, (IEnumerable<string>) value);
         }
 
