d62bb0de5ec21b28c3542af69cf6f3fcad994a8f
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-12 12:17:18 +0200

    Create edit page for Orders

---
+++ HibernatingRhinos.Orders.Backend/Features/Orders/Edit.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace HibernatingRhinos.Orders.Backend.Features.Orders
+{
+    public partial class Edit : Page
+    {
+        public Edit()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
