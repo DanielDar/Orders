11e9ee80bfaabdfe7117d2ee233d8fe43ff4cd4a
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-12 12:15:53 +0200

    Added AddressToMultiLineStringConverter

---
+++ HibernatingRhinos.Orders.Backend/Converters/AddressToMultiLineStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Linq;
+using HibernatingRhinos.Orders.Backend.Features.Orders;
+
+namespace HibernatingRhinos.Orders.Backend.Converters
+{
+    public class AddressToMultiLineStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var address = value as Address;
+            if (address == null)
+                return null;
+
+            return string.Format(@"Address1: {0}
+Address2: {1}
+City: {2}
+State/Province: {3}
+Zip/Postal Code: {4}
+Country: {5}", address.Address1, address.Address2, address.City, address.State, address.ZipCode, address.Country);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string) value).Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
