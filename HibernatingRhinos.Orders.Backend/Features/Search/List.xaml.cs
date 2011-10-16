67fdd85719f72c971813bbf83cd7346fae235d60
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-16 14:20:03 +0200

    add the view for search list (no binding yet)

---
+++ HibernatingRhinos.Orders.Backend/Features/Search/List.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace HibernatingRhinos.Orders.Backend.Features.Search
+{
+    public partial class List : Page
+    {
+        public List()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
